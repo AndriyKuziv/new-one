66e82de3001d96ca57e8589067c007e212e4f134
Andrii Kuziv <happyandreas18@gmail.com>
2022-07-04 09:49:40 +0300

    general Kenobi!

--- point-line.py
+++ point-line.py
@@ -4,7 +4,7 @@ x, y, x1, y1, x2, y2 = map(int, input().split())
  
 a = ( ((x - x1) * (x2 - x1)) + ((y - y1) * (y2 - y1)) ) / ( pow(x2 - x1, 2) + pow(y2 - y1, 2) )
 if a < 0: a = 0
-elif a > 1: a = 1
 
 res = math.sqrt( pow((x1 - x) + (a * (x2 - x1)), 2) + pow((y1 - y) + (a * (y2 - y1)), 2) )
+res = res + 1
 print("hehe")
